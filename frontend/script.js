e6106688c9c0dac0547c1479cb3c371b3cea6148
Alper Benzer <118021252+alperbenzer@users.noreply.github.com>
2025-05-05 11:46:18 +0300

    initial

---
+++ frontend/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("login-form");
+  const errorEl = document.getElementById("error");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    errorEl.textContent = "";
+
+    const username = form.username.value;
+    const password = form.password.value;
+
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    try {
+	const res = await fetch("http://localhost:8001/login", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        errorEl.textContent = data.detail || "Giriş başarısız";
+        return;
+      }
+
+      localStorage.setItem("token", data.access_token);
+      window.location.href = "calls.html";
+    } catch (err) {
+      errorEl.textContent = "Sunucuya bağlanılamadı.";
+    }
+  });
+});
